0d85d343dc1464600a3ac304847e5f30273e9d9a
ainsual <karimiyarullin23@gmail.com>
2024-12-04 13:31:34 +0300

    Yarullin

--- src/main/java/ru/git_task/Message.java
+++ src/main/java/ru/git_task/Message.java
@@ -1,4 +1,53 @@
 package ru.git_task;
 
 public class Message {
+    private User sender;
+    private User recipient;
+    private String textMessage;
+    private String timeAndDate;
+
+    public Message() {}
+
+    public Message(User sender, User recipient, String textMessage,String timeAndDate) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.textMessage = textMessage;
+        this.timeAndDate = timeAndDate;
+    }
+
+    //Gets
+    public String getTextMessage() {
+        return textMessage;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public String getTimeAndDate() {
+        return timeAndDate;
+    }
+
+    public void setTextMessage(String textMessage) {
+        this.textMessage = textMessage;
+    }
+
+    //Sets
+
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public void setTimeAndDate(String timeAndDate) {
+        this.timeAndDate = timeAndDate;
+    }
 }
